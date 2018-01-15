918b793bcdc4d8ed608d930c8688871777979958
shashi <sasitech18@gmail.com>
2018-01-15 03:43:33 +0000

    Keys,Implementing Time Travel and Wrapping Up

--- src/index.js
+++ src/index.js
@@ -97,25 +97,40 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-       const squares = this.state.squares.slice();
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
        if(calculateWinner(squares) || squares[i]) {
           return;
         }
          squares[i] = this.state.xIsNext ? 'X' : 'O';
          this.setState({
-           squares: squares,
+           history: history.concat([{
+            squares: squares
+           }]),
+           stepNumber: history.length,
            xIsNext: !this.state.xIsNext, 
         });
        }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -123,7 +138,7 @@ class Game extends React.Component {
         'Go to move #' + move :
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
