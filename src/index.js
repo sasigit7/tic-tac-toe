24cc59962c3c97829887400061463567ed306529
shashi <sasitech18@gmail.com>
2018-01-15 02:21:38 +0000

    Converted Square Component into a Functional Component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-         className="square" 
-         onClick={() => this.props.onClick()}>
-         {this.props.value}
-      </button>
-    );
-  }
+//Functional Component: 
+function Square(props) {
+  return (
+     <button 
+        className="square"
+        onClick={() => props.onClick()}>
+         {props.value}
+        </button>
+   );
 }
+
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button 
+//          className="square" 
+//          onClick={() => this.props.onClick()}>
+//          {this.props.value}
+//       </button>
+//     );
+//   }
+// }
 
 class Board extends React.Component {
   constructor(props) {
